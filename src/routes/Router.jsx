3cbfd79d200bfd243e1421d44cb6a4a867ccbcd7
Abdul Barik <abdulbarik1997m@gmail.com>
2025-09-14 03:08:56 +0600

    initial setup done

---
+++ src/routes/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage ";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
